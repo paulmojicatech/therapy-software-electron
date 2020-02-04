4076e744efbe1afb04e4d075c8d251ed47d0858d
Paul <paulmojicatech@gmail.com>
2020-02-04 06:20:38 -0500

    re add login service

--- src/app/user/services/login.service.ts
+++ src/app/user/services/login.service.ts
@@ -4,25 +4,25 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { ResultStatus } from '../models/userModel';
-// import { LoginUri } from 'src/env';
+import { LoginUri } from 'src/env';
 
 @Injectable()
 export class LoginService {
     constructor(private _http:HttpClient){ }
 
-    // public Login(details: any): Observable<ResultStatus> {
-    //     let headers: Headers = new Headers();
-    //     headers.append('Content-Type', 'application/json');
-    //     let opts = new RequestOptions({ headers: headers, body: details });
-    //     return this._http.post<ResultStatus>(LoginUri, opts).pipe(
-    //         tap((res) => {
-    //             localStorage.setItem('session-token', res.Message);
-    //         }),
-    //         catchError(err => {
-    //             return throwError(err);
-    //         })
-    //     );
+    public Login(details: any): Observable<ResultStatus> {
+        let headers: Headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        let opts = new RequestOptions({ headers: headers, body: details });
+        return this._http.post<ResultStatus>(LoginUri, opts).pipe(
+            tap((res) => {
+                localStorage.setItem('session-token', res.Message);
+            }),
+            catchError(err => {
+                return throwError(err);
+            })
+        );
 
-    //     //return of(<ResultStatus>{Type: 1, Message: 'Kirstin'});
-    // }
+        //return of(<ResultStatus>{Type: 1, Message: 'Kirstin'});
+    }
 }
