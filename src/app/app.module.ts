4b9540377fcc5e4062f35278b2f8de123895f96d
Paul Mojica <paulmojicatech@gmail.com>
2024-10-06 06:46:56 -0400

    core cli --> 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,7 +51,7 @@ const appRoutes: Route[] = [
     ],
     imports: [
         BrowserModule,
-        RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' }),
+        RouterModule.forRoot(appRoutes, {}),
         FormsModule,
         MatInputModule,
         MatFormFieldModule,
